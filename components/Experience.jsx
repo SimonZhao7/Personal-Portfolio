ddea392079508e4036a3fa91b4f8cab055f8fb06
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2022-10-04 22:53:52 -0700

    Responsive Experiences Section

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -4,11 +4,14 @@ import { experience } from '../constants/experience'
 
 const Experience = () => {
     return (
-        <section id='experience' className='h-[calc(100vh-72px)] flex flex-col justify-center'>
-            <h1 className='mb-4 text-3xl uppercase tracking-widest text-green-900 underline decoration-underline px-5 xs:px-10'>
+        <section
+            id='experience'
+            className='flex h-[calc(100vh-72px)]  flex-col justify-center 2xl:max-w-[1536px] mx-auto'
+        >
+            <h1 className='mb-4 px-5 text-3xl uppercase tracking-widest text-green-900 underline decoration-underline xs:px-10 xl:text-4xl'>
                 Experience
             </h1>
-            <div className='flex snap-x snap-mandatory space-x-4 xs:space-x-6 overflow-x-scroll pl-5 pr-10 xs:pl-10 xs:pr-16'>
+            <div className='flex min-h-max snap-x snap-mandatory space-x-4 overflow-x-scroll pl-5  pr-10 xs:space-x-6 xs:pl-10 xs:pr-16 xl:w-full xl:pr-10'>
                 {experience.map((e) => {
                     const { id, image, company, position, details } = e
                     return (
--- components/WorkExperience.jsx
+++ components/WorkExperience.jsx
@@ -4,24 +4,28 @@ import { motion } from 'framer-motion'
 
 const WorkExperience = ({ image, company, position, details }) => {
     return (
-        <div className='min-h-full min-w-full snap-center snap-always space-y-6 rounded-lg bg-[#A8B888] p-8 xs:p-10 shadow-lg hover:brightness-110 transition duration-100 ease'>
+        <div className='snap-center ease min-h-full min-w-full snap-always space-y-6 rounded-lg bg-[#A8B888] p-8 shadow-lg transition duration-100 hover:brightness-110 xs:p-10 md:min-w-[500px] xl:flex-1 xl:min-w-0'>
             <div className='flex w-full justify-center'>
                 <motion.div
                     initial={{ y: -50, opacity: 0 }}
                     whileInView={{ y: 0, opacity: 1 }}
                     transition={{ duration: 0.8 }}
                     viewport={{ once: true }}
-                    className='w-24 xs:w-32'
+                    className='w-24 xs:w-32 md:w-38'
                 >
-                    <Image src={image} className='rounded-full' alt={company + ' logo'} />
+                    <Image
+                        src={image}
+                        className='rounded-full'
+                        alt={company + ' logo'}
+                    />
                 </motion.div>
             </div>
             <article className='space-y-2'>
-                <h1 className='text-xl xs:text-2xl'>{position}</h1>
-                <h3 className='text-sm xs:text-base italic'>{company}</h3>
+                <h1 className='text-xl xs:text-2xl md:text-2xl 2xl:text-3xl'>{position}</h1>
+                <h3 className='text-sm italic xs:text-base md:text-lg 2xl:text-xl'>{company}</h3>
                 <ul className='list-disc space-y-2'>
                     {details?.map((d, i) => (
-                        <li className='w-full break-words text-sm' key={i}>
+                        <li className='w-full break-words text-sm 2xl:text-xl' key={i}>
                             {d}
                         </li>
                     ))}
