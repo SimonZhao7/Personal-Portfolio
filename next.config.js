09809816be8452f161fd195b0b23670e92fa1c2f
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2023-03-19 20:31:55 -0700

    Added English Project Redirect On Domain

--- next.config.js
+++ next.config.js
@@ -3,8 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['cdn.jsdelivr.net']
-  }
+      domains: ['cdn.jsdelivr.net'],
+  },
+  redirects: async () => {
+    return [
+      {
+        source: '/writers-nbk',
+        destination: 'https://writers-notebook.vercel.app/',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
